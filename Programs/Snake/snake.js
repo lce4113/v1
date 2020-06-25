df3f5c10a351791b2fc597dc21a677f5fb1d5bc1
Om Mahesh <46605803+om-mahesh@users.noreply.github.com>
2020-06-25 00:39:58 -0700

    First Commit!

---
+++ Programs/Snake/snake.js
@@ -0,0 +1,145 @@
+var squares, gridSize, status, dir, dirStack, framesSinceDeath, eaten;
+
+function startGame() {
+
+  gridSize = floor(min(width, height)*1/20);
+  status = "playing";
+  squares = new Array(2).fill( createVector(width%gridSize/2 + gridSize*floor(width/gridSize/2), height%gridSize/2 + gridSize*floor(height/gridSize/2)) );
+  dir = createVector(0, 0);
+  dirStack = [];
+  framesSinceDeath = 0;
+  eaten = true;
+  applePos = createVector( width%gridSize/2 + gridSize*floor(random(floor(width/gridSize))), height%gridSize/2 + gridSize*floor(random(floor(height/gridSize))) )
+
+}
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+  startGame();
+
+}
+
+function draw() {
+
+  // Draw background
+  noStroke();
+  background(175, 255, 175);
+  for (let i = 0; i < floor(width/gridSize); i++) {
+    for (let j = 0; j < floor(height/gridSize); j++) {
+      if ( (i+j)%2 == 0 ) {
+        fill(150, 200, 50);
+      } else {
+        fill(175, 215, 100);
+      }
+      rect( width%gridSize/2 + i*gridSize, height%gridSize/2 + j*gridSize, gridSize, gridSize );
+    }
+  }
+
+  // Draw apple
+  fill(255, 0, 0);
+  circle( applePos.x + gridSize/2, applePos.y + gridSize/2, gridSize*3/4 );
+  stroke(151, 111, 76);
+  strokeWeight(gridSize*1/15);
+  line(applePos.x+gridSize/2, applePos.y+gridSize*0.1, applePos.x+gridSize/2, applePos.y+gridSize*0.4);
+
+  // Draw snake
+  noStroke();
+  fill(0, 150, 0);
+  circle( squares[0].x + gridSize/2, squares[0].y + gridSize/2, gridSize*sqrt(2) );
+  for (let i = 1; i < squares.length-1; i++) {
+    rect( squares[i].x, squares[i].y, gridSize, gridSize );
+  }
+
+  if ( status == "playing" ) {
+
+    // Frame rate is slow
+    frameRate(7.5);
+
+    // Use next dir in dirStack
+    if ( dirStack.length > 0 ) {
+      if ( dirStack[0].x != -dir.x || dirStack[0].y != -dir.y || squares.length <= 3 ) {
+        dir = dirStack[0];
+      }
+      dirStack.shift();
+    }
+
+    // Move snake
+    if ( abs(applePos.x - squares[0].x) <= 0.01 && abs(applePos.y - squares[0].y) <= 0.01 ) {
+      applePos = createVector( width%gridSize/2 + gridSize*floor(random(floor(width/gridSize))), height%gridSize/2 + gridSize*floor(random(floor(height/gridSize))) );
+    } else {
+      squares.pop();
+    }
+    squares.unshift( p5.Vector.add(squares[0], dir) );
+
+    // Check if snake hit walls
+    if ( width-width%gridSize/2-gridSize < squares[0].x || squares[0].x < width%gridSize/2 ||
+         height-height%gridSize/2-gridSize < squares[0].y || squares[0].y < height%gridSize/2 ) {
+      squares.shift();
+      squares.push( squares[ squares.length-1 ] );
+      status = "end screen";
+    }
+
+    // Check if snake hit self
+    for (let i = 1; i < squares.length-1; i++) {
+      if ( squares[0].x == squares[i].x && squares[0].y == squares[i].y ) {
+        squares.shift();
+        squares.push( squares[ squares.length-1 ] );
+        status = "end screen";
+      }
+    }
+
+  } else if ( status == "end screen" || status == "not playing" ) {
+
+    // Frame rate is fast
+    frameRate(60);
+    framesSinceDeath++;
+
+    // White tint
+    background(255, 255, 255, min(100, framesSinceDeath*3));
+
+    // "You scored __" and "Click anywhere to play again."
+    noStroke();
+    textFont("Georgia", height*1/8);
+    textAlign(CENTER, BOTTOM);
+    fill(0, 0, 0, min(255, framesSinceDeath*3));
+    text( "You Scored " + str(squares.length-1), width*1/2, height*1/2 );
+    textSize(height*1/16);
+    textAlign(CENTER, TOP);
+    text( "Click anywhere to play again.", width*1/2, height*1/2 );
+
+    // Switch to not playing from end screen allowing user to play again
+    if ( framesSinceDeath*3 == 255 ) {
+      status = "not playing";
+    }
+
+    // If user clicks on screen play again
+    if ( status == "not playing" && mouseIsPressed ) {
+      startGame();
+    }
+
+  }
+
+}
+
+function keyTyped() {
+  
+  if ( status == "playing" ) {
+    if ( key == "i" || key == "w" || keyCode == UP_ARROW ) {
+      dirStack.push(createVector(0, -gridSize));
+    } else if ( key == "j" || key == "a" || keyCode == LEFT_ARROW ) {
+      dirStack.push(createVector(-gridSize, 0));
+    } else if ( key == "k" || key == "s" || keyCode == DOWN_ARROW ) {
+      dirStack.push(createVector(0, gridSize));
+    } else if ( key == "l" || key == "d" || keyCode == RIGHT_ARROW ) {
+      dirStack.push(createVector(gridSize, 0));
+    } else if ( key == " " ) {
+      status = "paused";
+    }
+  } else if ( status == "paused" ) {
+    status = "playing";
+  } else if ( status == "not playing" ) {
+    startGame();
+  }
+
+}
